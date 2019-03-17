f2237f66f3c7715919b792adc61a7b95639113f9
Matheus <matheus.silva1308@etec.sp.gov.br>
2019-03-17 20:55:07 -0300

    Criação da solução

---
+++ Sovamo/Sovamo/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Sovamo.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("bancosovamo") { Configuration.ProxyCreationEnabled = false; Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>()); }
+
+
+        //           Classes aqui
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) { modelBuilder.Conventions.Remove(); modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); }
+    }
+}
