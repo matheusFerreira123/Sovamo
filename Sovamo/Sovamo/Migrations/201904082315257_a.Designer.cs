303efeb121891d2c00b0b176c7ab1641647f0e47
Jonathas <Jonathas@DESKTOP-VKT91MU>
2019-04-09 19:44:29 -0300

    asas

--- Sovamo/Sovamo/Migrations/201904082315257_a.Designer.cs
+++ Sovamo/Sovamo/Migrations/201904082315257_a.Designer.cs
@@ -7,13 +7,13 @@ namespace Sovamo.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class sovai : IMigrationMetadata
+    public sealed partial class a : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(sovai));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(a));
         
         string IMigrationMetadata.Id
         {
-            get { return "201904071940341_sovai"; }
+            get { return "201904082315257_a"; }
         }
         
         string IMigrationMetadata.Source
